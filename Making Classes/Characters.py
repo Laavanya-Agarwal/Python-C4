700a30a7c3cd34c9c297cbe4d1352294a0030679
Laavanya-Agarwal <71713140+Laavanya-Agarwal@users.noreply.github.com>
2021-08-27 18:26:12 +0530

    Update Characters.py

--- Making Classes/Characters.py
+++ Making Classes/Characters.py
@@ -1,8 +1,6 @@
 class characters(object):
-    def __init__(self, user, cardNum, pinNum, gender, yearBornIn):
+    def __init__(self, user, gender, yearBornIn):
         self.user = user
-        self.cardNum = cardNum
-        self.pinNum = pinNum
         self.gender = gender
         self.yearBornIn = yearBornIn
 
@@ -16,10 +14,10 @@ class characters(object):
         print('''This person's age is ''' + str(ageOfPerson))
 
 # objects
-Eleanor = characters('Eleanor', 1001, 4444, 'female', 1982)
-Chidi = characters('Chidi', 1002, 5555, 'male', 1983)
-Jason = characters('Jason', 1003, 3001, 'male', 2007)
-Tahani = characters('Tahani', 1004, 2735, 'female', 1986)
+Eleanor = characters('Eleanor', 'female', 1982)
+Chidi = characters('Chidi', 'male', 1983)
+Jason = characters('Jason', 'male', 2007)
+Tahani = characters('Tahani', 'female', 1986)
 
 # using functions and objects
 Eleanor.presentInfo()
